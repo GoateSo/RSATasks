b6bb48ca259722f22970bbbe047db5e9e3fc9465
GoateSo <59705775+GoateSo@users.noreply.github.com>
2021-01-10 19:27:49 -0600

    Create testgen.lua

---
+++ sliding/testgen.lua
@@ -0,0 +1,20 @@
+--[[
+Autogenerates test cases for sliding problem
+
+output: table of three values:
+	arr, an array with size in range [0,5000], and with elements in range [-100,100] (hard coded limits; ideally should work for arbitrary values and arbitrary sizes)
+	width, a positive integer in range [0,100]
+	step, a positive integer in range [0,100]
+]]
+local function mkTests(n)
+    n = n or 100
+    local ret = {}
+    for i = 1, n do
+        local t = {}
+        for i = 1, math.random(0,5000) do
+            t[i] = math.random(-100,100) 
+        end
+        ret[i] = {t,math.random(0,100),math.random(0,100)}
+    end
+    return ret
+end
