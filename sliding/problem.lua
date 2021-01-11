09818c3d2a81d75626a513dc701418a60c8dc6f2
GoateSo <59705775+GoateSo@users.noreply.github.com>
2021-01-10 21:10:26 -0600

    Update problem.lua

--- sliding/problem.lua
+++ sliding/problem.lua
@@ -1,18 +1,20 @@
 --[[
 Challenge 3 - sliding window
  
-directions: Groups elements of the array in fixed size blocks by passing a "sliding window" over them
+directions: Groups elements of the array in fixed size blocks by passing a "sliding window" (https://tinyurl.com/slidinwindow) over them
  
 inputs: arr, the array which the sliding window is passed over
         width, the number of elements per group
         step, the distance between the first elements of each group
- 
+
+output: an array of arrays of size `width`, except the last array, which may be smaller
+
 examples:
 sliding({1, 2, 3, 4}, 3, 1) → {{1, 2, 3}, {2, 3, 4}}
 sliding({1, 2, 3, 4, 5}, 2, 2) → {{1, 2}, {3, 4}, {5}}
 sliding({1, 2, 3, 4, 5, 6}, 2, 3) → {{1, 2}, {4, 5}}
 ]]
-local mkTests = require "testgen"
+local mkTests = require(script.parent.testgen)
 
 --template
 local function sliding(arr, width, step)
@@ -23,4 +25,5 @@ end
 for _,v in ipairs(mkTests()) do
     local arr, width, step = table.unpack(v)
     local out = sliding(arr, width, step)
+    print("passed")
 end
