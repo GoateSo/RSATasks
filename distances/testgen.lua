2cc22deab3371ebf81d14fed1c9d7ed52847889e
GoateSo <59705775+GoateSo@users.noreply.github.com>
2021-01-10 21:47:11 -0600

    Update testgen.lua

--- distances/testgen.lua
+++ distances/testgen.lua
@@ -4,7 +4,7 @@ Autogenerates test cases for distance problem
 output: 
     possibly empty arrays of between 0 and 50 vector3s, with each of the X, Y, and Z values in range [-100,100]
 ]]
-local function mkTests(n)
+return function(n)
 	n = n or 100
 	local ret = {}
 	for i=1,n do
