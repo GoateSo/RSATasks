0cf3b971d33220c1fbaff55f96cd163fc679fe08
GoateSo <59705775+GoateSo@users.noreply.github.com>
2021-01-10 20:51:45 -0600

    Update testgen.lua

--- squares/testgen.lua
+++ squares/testgen.lua
@@ -4,8 +4,7 @@ Autogenerates test cases for sliding problem
 output:
   limit, integer in range [0,500]
 ]]
-
-local function mkTests(n)
+return function(n)
     n = n or 100
     local t = {}
     for i = 1, n do
