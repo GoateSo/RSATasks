26d0fb64b4f3b8b3eb20f6c14c479525cc0f8266
GoateSo <59705775+GoateSo@users.noreply.github.com>
2021-01-10 20:20:43 -0600

    Create testgen.lua

---
+++ squares/testgen.lua
@@ -0,0 +1,15 @@
+ --[[
+Autogenerates test cases for sliding problem
+
+output:
+  limit, integer in range [0,500]
+]]
+
+local function mkTests(n)
+    n = n or 100
+    local t = {}
+    for i = 1, n do
+        t[i] = math.random(0,500)
+    end
+    return t
+end
