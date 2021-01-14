3759be60231f0b61e4874b5c0cdb26366430e2d4
GoateSo <59705775+GoateSo@users.noreply.github.com>
2021-01-14 10:00:55 -0600

    Update problem.lua

--- escaped string/problem.lua
+++ escaped string/problem.lua
@@ -1,3 +1,18 @@
+--[[
+directions: given a string containing 2 unescaped quotation marks(") and any number of escaped quotation marks (\"), find the string of characters between the unescaped quotation marks
+
+input: a string containing 2 unescaped quotes and any number of escaped quotes
+
+output: a string of the character between the unescaped quotes
+
+example:
+	(Bob told me: "jimmy says \"Hello\"") -->
+yields
+	(jimmy says \"Hello\")
+since
+	while there are quotations around "Hello", they're escaped so they can neither act as starting quotes nor ending quotes, and are instead treated as literal characters
+]]
+
 local mkTests = require(script.Parent.testgen)
 
 --template
