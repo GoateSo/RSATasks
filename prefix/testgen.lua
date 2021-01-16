9a98cb9f76dff0b54a606f62dfb2f5e1f3316d9e
GoateSo <59705775+GoateSo@users.noreply.github.com>
2021-01-15 23:57:17 -0600

    Update testgen.lua

--- prefix/testgen.lua
+++ prefix/testgen.lua
@@ -7,32 +7,32 @@ output:
 local rExpr
 
 local function rDigit()
-	return tostring(math.random(0,9))
+    return tostring(math.random(0,9))
 end
 
 local function rSum()
-	return "&".. rExpr()
+    return "&".. rExpr()
 end
 
 local function rAdd()
-	return "+".. rDigit().. rExpr()
+    return "+".. rDigit().. rExpr()
 end
 
 local function rSub()
-	return "-".. rDigit().. rExpr()
+    return "-".. rDigit().. rExpr()
 end
 
 function rExpr()
-	local r = math.random()
-	if r < 0.25 then
-		return rDigit()
-	elseif r >= 0.25 and r < 0.4 then
-		return rSum()
-	elseif r >= 0.4 and r < 0.7 then
-		return rAdd()
-	else
-		return rSub()		
-	end
+    local r = math.random()
+    if r < 0.25 then
+        return rDigit()
+    elseif r >= 0.25 and r < 0.4 then
+        return rSum()
+    elseif r >= 0.4 and r < 0.7 then
+        return rAdd()
+    else
+        return rSub()		
+    end
 end
 
 return function(n)
