ef558fa26e4186dab330f76ff5aee87599c9d0a8
GoateSo <59705775+GoateSo@users.noreply.github.com>
2021-01-15 23:57:59 -0600

    Update problem.lua

--- escaped string/problem.lua
+++ escaped string/problem.lua
@@ -17,10 +17,10 @@ local mkTests = require(script.Parent.testgen)
 
 --template
 local function getString(str)
-	return ""
+    return ""
 end
 
 --testing template
 for _,str in ipairs(mkTests()) do
-	local res = getString(str)
+    local res = getString(str)
 end
