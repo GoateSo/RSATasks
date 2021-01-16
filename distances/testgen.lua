b56dee0d4e33419bf78f83ca51dc0fa3d2068543
GoateSo <59705775+GoateSo@users.noreply.github.com>
2021-01-15 23:59:11 -0600

    Update testgen.lua

--- distances/testgen.lua
+++ distances/testgen.lua
@@ -5,14 +5,14 @@ output:
     possibly empty arrays of vector3s
 ]]
 return function(n)
-	n = n or 100
-	local ret = {}
-	for i=1,n do
-		ret[i] = {}
-		for j=1,math.random(0,50) do
-			ret[i][j] = Vector3.new(math.random(-100,100),math.random(-100,100),math.random(-100,100))
-		end
-	end
-	return ret
+    n = n or 100
+    local ret = {}
+    for i=1,n do
+        ret[i] = {}
+        for j=1,math.random(0,50) do
+            ret[i][j] = Vector3.new(math.random(-100,100),math.random(-100,100),math.random(-100,100))
+        end
+    end
+    return ret
 end
 
