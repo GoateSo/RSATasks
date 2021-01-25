50ae27c3f8f1b170cd9d6fb4603f9742e56d2a24
GoateSo <59705775+GoateSo@users.noreply.github.com>
2021-01-24 21:06:25 -0600

    Update problem.lua

--- escaped string/problem.lua
+++ escaped string/problem.lua
@@ -6,9 +6,9 @@ input: a string containing 2 unescaped quotes and any number of escaped quotes
 output: a string of the character between the unescaped quotes
 
 example:
-	(Bob told me: "jimmy says \"Hello\"") -->
+	Bob told me: "jimmy says \"Hello\"" -->
 yields
-	(jimmy says \"Hello\")
+	jimmy says \"Hello\"
 since
 	while there are quotations around "Hello", they're escaped so they can neither act as starting quotes nor ending quotes, and are instead treated as literal characters
 ]]
