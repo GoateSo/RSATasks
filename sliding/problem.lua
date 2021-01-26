62caf0ce7de51972f6baaa9fa59e0d05fd5952b9
GoateSo <59705775+GoateSo@users.noreply.github.com>
2021-01-26 00:11:19 -0600

    change rules and examples

--- sliding/problem.lua
+++ sliding/problem.lua
@@ -1,12 +1,11 @@
 --[[
-directions: Groups elements of the array in fixed size blocks by passing a "sliding window" over them, stopping when reaching the end of the collection 
-            (when all elements are exhausted)
+directions: Groups elements of the array in fixed size blocks by passing a "sliding window" over them, stopping when the number of elements in the collection is smaller than the  width
  
 inputs: arr, the array which the sliding window is passed over
         width, the number of elements per group
         step, the distance between the first elements of each group
 
-output: an array of arrays of size `width`, except the last array, which may be smaller
+output: an array of arrays of size `width`
 
 example:
     sliding({1, 2, 3, 4}, 2, 1) → {{1, 2}, {2, 3}, {3, 4}}
@@ -19,7 +18,7 @@ example:
     +--------------+
 
 additional examples:
-    sliding({1, 2, 3, 4, 5}, 2, 2) → {{1, 2}, {3, 4}, {5}}
+    sliding({1, 2, 3, 4, 5}, 2, 2) → {{1, 2}, {3, 4}}
     sliding({1, 2, 3, 4, 5, 6}, 2, 3) → {{1, 2}, {4, 5}}
 ]]
 local mkTests, testCases = table.unpack(require(script.Parent.testgen))
