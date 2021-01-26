73177b73f0a12f59a90cd1d74c821ae33eaa527c
Jade Piramide <jadepiramidepogi@gmail.com>
2021-01-25 16:53:18 -0800

    Changed scanning's assertion format to fit with given inputs.

--- scanning/problem.lua
+++ scanning/problem.lua
@@ -48,5 +48,11 @@ end
 
 for input, output in pairs(testCases) do
     local res = scanRight(unpack(input))
-    assert(eq(res,output),("failed for %q, expected {%s} got {%s}"):format(input,table.concat(output,", "),table.concat(res,", ")))
+    assert(eq(res,output),("failed for ({%s}, %d, %q), expected {%s} got {%s}"):format(
+        table.concat(input[1], ", "),
+        input[2],
+        tostring(input[3])    
+        table.concat(output,", "),
+        table.concat(res,", "))
+    )
 end
