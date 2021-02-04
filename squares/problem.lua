d37e69ebb8c2c852d0069151510df54180303ab7
GoateSo <59705775+GoateSo@users.noreply.github.com>
2021-02-03 23:32:40 -0600

    change naming to alleviate confusion

--- squares/problem.lua
+++ squares/problem.lua
@@ -2,7 +2,7 @@
 directions: implement a stateless iterator function which outputs the squares of all even natural numbers up to a given number
  
 inputs: limit, a positive integer at which iteration should stop
-        cur, a nullable positive integer which represents the previous number 
+        prev, a nullable positive integer which represents the previous number 
 
 example: 
     for i, s in squares, 8 do
@@ -17,7 +17,7 @@ results in:
 local mkTests, testCases = table.unpack(require(script.Parent.testgen))
 
 --template
-local function squares(limit, cur)
+local function squares(limit, prev)
     return
 end
  
@@ -29,12 +29,12 @@ for _, lim in ipairs(mkTests()) do
 end
 
 for input, output in ipairs(testCases) do
-    local lim,cur = table.unpack(input)
+    local lim,prev = table.unpack(input)
     local r1,r2 = squares(lim,cur)
     local o1,o2 = table.unpack(output)
     assert(r1 == o1 and o2 == r2, ("expected %s and %s for %s and %s, instead got %s and %s"):format(
         o1, o2,
-        lim, cur,
+        lim, prev,
         r1, r2
     ))
 end
