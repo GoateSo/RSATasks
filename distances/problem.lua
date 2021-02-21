b6ca0223fb6b6d82040496a97b47e32b541148e9
GoateSo <59705775+GoateSo@users.noreply.github.com>
2021-02-21 14:23:20 -0600

    fix typo

--- distances/problem.lua
+++ distances/problem.lua
@@ -7,7 +7,7 @@ Output: the distance between the closest pair of points, or 0 if there are less 
 Example:
 	if points = { Vector3.new(0, 5, 10), Vector3.new(10, 10, 10), Vector3.new(0, 5, 7) },
 	then minimum_distance(points) yields 3,
-	because the distance between the first two points is ~11.575837,
+	because the distance between the first two points is ~11.180340,
 	and the distance between the first and third point is 3,
 	and the distance between the second and third point is ~11.575837.
 ]]
