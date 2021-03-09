1361a41bc01eb619e21777d6d1167bef515e6cd6
GoateSo <59705775+GoateSo@users.noreply.github.com>
2021-03-09 00:33:02 -0600

    remove redundant code

--- prefix/testgen.lua
+++ prefix/testgen.lua
@@ -3,37 +3,22 @@ Autogenerates test cases for prefix problem
 output:
 	a prefix expression of arbitrary length with an arbitrary combination of the operators listed in problem.lua
 ]]
-local rExpr
-
 local function rDigit()
-    return tostring(math.random(0,9))
-end
-
-local function rSum()
-    return "&".. rExpr()
-end
-
-local function rAdd()
-    return "+".. rDigit().. rExpr()
-end
-
-local function rSub()
-    return "-".. rDigit().. rExpr()
+	return tostring(math.random(0,9))
 end
 
 function rExpr()
-    local r = math.random()
-    if r < 0.25 then
-        return rDigit()
-    elseif r >= 0.25 and r < 0.4 then
-        return rSum()
-    elseif r >= 0.4 and r < 0.7 then
-        return rAdd()
-    else
-        return rSub()		
-    end
+	local r = math.random()
+	if r < 0.25 then
+		return rDigit()
+	elseif r >= 0.25 and r < 0.4 then
+		return  "&".. rExpr()
+	elseif r >= 0.4 and r < 0.7 then
+		return "+".. rDigit().. rExpr()
+	else
+		return "-".. rDigit().. rExpr()		
+	end
 end
-
 
 
 return { function(n)
