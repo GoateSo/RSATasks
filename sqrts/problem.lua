5ab0355cf4d4dda618d39efd94a4b0c41fc42959
GoateSo <59705775+GoateSo@users.noreply.github.com>
2021-04-07 13:53:28 -0500

    add necessary hints & minor testing changes

--- sqrts/problem.lua
+++ sqrts/problem.lua
@@ -1,11 +1,14 @@
 --[[
 directions: generate a list of approixmations for the square root of a number given an inital approximation until the approximations are within a certain bound
- 
+
 inputs: num, a positive integer to get the square root of
-        init, the inital approximation (positive integer)
+        approx, the inital approximation (positive integer)
         bound, an acceptable error from the actual root
 
 output: approxs, a list of approximations of the square root
+
+hint: you can generate a better approximation using the formula:
+      next = (approx + (num / approx)) / 2
 
 example: 
     approxSqrts(3, 1, 1e-6)
@@ -19,17 +22,17 @@ since
 local mkTests, testCases = table.unpack(require(script.Parent.testgen))
 
 --template
-local function approxSqrts(num, init, bound)
+local function approxSqrts(num, approx, bound)
     return {}
 end
  
 --testing 
 for input, output in ipairs(testCases) do
-    local num,init,bound = table.unpack(input)
-    local approxs = approxSqrts(num,init,bound)
+    local num,approx,bound = table.unpack(input)
+    local approxs = approxSqrts(num,approx,bound)
     local answers = table.unpack(output)
     assert(#answers == #approxs, ("expected %d approxmations, got %d instead"):format(#answers,#approxs))
     for i, v in ipairs(answers) do
-        assert(v == approxs[i], ("expected %f for approximation #%d, got %d instead"):format(v,i,approxs[i]))
+        assert(math.abs(v - approxs[i])<1e-8, ("expected %f for approximation #%d, got %d instead"):format(v,i,approxs[i]))
     end
 end
