e306759c3b2b9f61fe76027a66a795cada50820a
GoateSo <59705775+GoateSo@users.noreply.github.com>
2021-08-27 19:37:59 -0500

    clean up code

--- scanning/problem.lua
+++ scanning/problem.lua
@@ -32,12 +32,6 @@ local function scanRight(arr, init, op)
 end
 
 --testing template
-for _,case in ipairs(mkTests()) do
-    local arr, init, op = table.unpack(case)
-    local results = scanRight(arr, init, op)
-    --...
-end
-
 local function eq(s1,s2)
     local r = #s1 == #s2
     for i,v in ipairs(s1) do
